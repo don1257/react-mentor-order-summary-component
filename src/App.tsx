0b00b3181d226fe745974f6c48419234ed0ae691
don1257 <45983245+don1257@users.noreply.github.com>
2023-08-16 14:29:24 -0400

    Adding background and media queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import mobileDesign from './design/mobile-design.jpg'
 import Card from "./components/Card/Card";
+import patternBackgroundDesktop from "./images/pattern-background-desktop.svg";
 
 function App() {
+
   return (
-    <div className='App'>
+    <div className='App' >
+        <img
+            src={patternBackgroundDesktop}
+            alt='Pattern background'
+            className='PatternBackground'
+        />
         <Card/>
-      <img
-        src={mobileDesign}
-      />
     </div>
   );
 }

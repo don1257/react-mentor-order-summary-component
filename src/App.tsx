0b16921085c84dd272754779a41fa9521c728bd4
don1257 <45983245+don1257@users.noreply.github.com>
2023-08-16 14:32:33 -0400

    Adding background svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from "./components/Card/Card";
 import patternBackgroundDesktop from "./images/pattern-background-desktop.svg";
+import patternBackgroundMobile from "./images/pattern-background-mobile.svg";
 
 function App() {
+    const [backgroundImage, setBackgroundImage] = useState(patternBackgroundMobile);
 
-  return (
-    <div className='App' >
-        <img
-            src={patternBackgroundDesktop}
-            alt='Pattern background'
-            className='PatternBackground'
-        />
-        <Card/>
-    </div>
-  );
+    const handleMediaQuery = () => {
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            setBackgroundImage(patternBackgroundMobile);
+        } else {
+            setBackgroundImage(patternBackgroundDesktop);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleMediaQuery);
+        return () => {
+            window.removeEventListener('resize', handleMediaQuery);
+        };
+    }, []);
+
+    return (
+        <div className='App' >
+            <img
+                src={backgroundImage}
+                alt='Pattern background'
+                className='PatternBackground'
+            />
+            <Card/>
+        </div>
+    );
 }
 
 export default App;

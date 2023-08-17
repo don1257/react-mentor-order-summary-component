1995055ffbd37cbc8a7bc21158ff52b24add9950
don1257 <45983245+don1257@users.noreply.github.com>
2023-08-17 12:59:51 -0400

    Removed absolutes and amended background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Card from "./components/Card/Card";
-import patternBackgroundDesktop from "./images/pattern-background-desktop.svg";
-import patternBackgroundMobile from "./images/pattern-background-mobile.svg";
-import desktopDesign from './design/desktop-design.jpg'
 
 function App() {
-    const [backgroundImage, setBackgroundImage] = useState('');
-
-    const handleMediaQuery = () => {
-        if (window.matchMedia("(max-width: 768px)").matches) {
-            setBackgroundImage(patternBackgroundMobile);
-        } else {
-            setBackgroundImage(patternBackgroundDesktop);
-        }
-    };
-
-    useEffect(() => {
-        handleMediaQuery(); // Set initial background image on load
-
-        window.addEventListener('resize', handleMediaQuery);
-        return () => {
-            window.removeEventListener('resize', handleMediaQuery);
-        };
-    }, []);
 
     return (
-        <div className='App' >
-            <img
-                src={backgroundImage}
-                alt='Pattern background'
-                className='PatternBackground'
-            />
+        <div className='App'>
             <Card/>
         </div>
-
     );
 }
 
